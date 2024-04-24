d6cd959a665a3e31fde9bdcf7e2d5a67e7db6b63
sebastian monsalve <sebastian041611@gmail.com>
2024-04-23 23:24:49 -0500

    creacion de recursos users

---
+++ api/src/user/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+  id: number;
+  
+  @Column()
+  firstName: string;
+  @Column()
+  lastName: string;
+  @Column()
+  Adress: string;
+  @Column()
+  phonenumber: string;
+  @Column()
+  email: string;
+  @Column({ default: true })
+  isActive: boolean;
+}
